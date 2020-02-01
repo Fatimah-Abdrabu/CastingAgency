72cb9f67f838b795cd59e2dc2b7e9c3a9c85f12e
Fatima Ahmed Abdo <fabdo@shawarmer.local>
2020-02-01 20:43:31 +0300

    added bash file

---
+++ setup.sh
@@ -0,0 +1,6 @@
+
+export JWT_SECRET = 'myjwtsecret'
+export AUTH0_DOMAIN = 'ifatimah.auth0.com'
+export ALGORITHMS = ['RS256']
+export API_AUDIENCE = 'capstone'
+export CLIENT_ID = 'fjyJhWw06AMWT7s3ZztqMF38dsH9uOxs'
